375eab4feb8da145a565f358bd23ba86ebcfd0f7
ddiablou <jekem13936@jo6s.com>
2023-03-11 20:40:53 +0100

    fix eslint!!!!!!!!!!

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,17 +16,7 @@ const config = {
     project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  ignorePatterns: [
-    "react-hooks/exhaustive-deps",
-    "@typescript-eslint/no-floating-promises",
-    "@typescript-eslint/restrict-template-expressions",
-    "@typescript-eslint/no-unused-vars",
-    "@typescript-eslint/no-non-null-assertion",
-    "@typescript-eslint/no-empty-interface",
-    "@typescript-eslint/no-misused-promises",
-    "jsx-a11y/alt-text",
-  ],
+  extends: ["next/core-web-vitals"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -35,6 +25,14 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
+    "react-hooks/exhaustive-deps": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "jsx-a11y/alt-text": "off",
   },
 };
 
