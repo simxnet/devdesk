6ccd92dc399f641a27552ff989b413e9c9d480cc
ddiablou <jekem13936@jo6s.com>
2023-03-18 19:42:20 +0100

    ඞ

--- src/components/parts/DevBadge.tsx
+++ src/components/parts/DevBadge.tsx
@@ -1,4 +1,4 @@
-import { IconLeaf } from "@tabler/icons-react";
+import { IconBrandAmongUs } from "@tabler/icons-react";
 import {
   Tooltip,
   TooltipContent,
@@ -12,7 +12,7 @@ export default function DevBadge({ color }: { color: string }) {
       <Tooltip>
         <TooltipTrigger asChild>
           <div className="ml-2 rounded-md bg-slate-900 p-[6px]">
-            <IconLeaf
+            <IconBrandAmongUs
               className="h-4 w-4"
               style={{
                 color,
@@ -21,7 +21,7 @@ export default function DevBadge({ color }: { color: string }) {
           </div>
         </TooltipTrigger>
         <TooltipContent className="!shadow-xl">
-          <p>Website developer</p>
+          <p>Website developer (sussy)</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
